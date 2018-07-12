5a74ab159d7b79a51d76fb1741e37f398920a7ec
Corinne Winthrop <corinne.winthrop@simuwatt.com>
2018-07-12 15:42:20 -0600

    Add redux and fix reducer errors

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,7 +1,29 @@
+import { ADD_ARTICLE } from "../constants/action-types";
+
 const initialState = {
   articles: []
 };
 
-const rootReducer = (state = initialState, action) => state;
+// // just returns the initial state
+// const rootReducer = (state = initialState, action) => state;
+
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_ARTICLE:
+      // // the below code is directly changing the original state object.
+      // // and is therefore breaking the immutability rule
+      // state.articles.push(action.payload);
+      // return state;
+      return { ...state, articles: [...state.articles, action.payload] };
+    default:
+      return state;
+  }
+};
+
+// const rootReducer = (state = initialState, action) => {
+//   const handler = ACTION_HANDLERS[action.type]
+//   return handler ? handler(state, action) : state
+// }
+
 
 export default rootReducer;
